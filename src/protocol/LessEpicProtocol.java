42e17fb8c9a793e0bc4e436bf9fc468ad33aeb55
Jasper Sustronk <j.j.sustronk11@hotmail.com>
2016-02-18 16:08:37 +0100

    update #over 9000

--- src/protocol/LessEpicProtocol.java
+++ src/protocol/LessEpicProtocol.java
@@ -1,72 +1,133 @@
 package protocol;
 
+import java.util.Random;
+
 import client.MACChallengeClient;
+import sun.management.snmp.util.MibLogger;
 
 /**
  * Created by Sander on 18/02/2016.
  */
 public class LessEpicProtocol implements IMACProtocol {
 
-    private int node = 0;
-    int count = -1;
-    int counter = 8;
-    int waitingTime = (int) Math.round(Math.random() * 4);
-    State state = State.INITIAL;
-    boolean tried = false;
+	
+	private static final int HEADER_LAST_PACKET = 12345;
+	
+	boolean[] nodesWithQueue = {false, false, false, false};
+	
+	private int node = 0;
+	int count = -1;
+	int counter = 0;
+	int waitingTime = (int) Math.round(Math.random() * 4);
+	State state = State.INITIAL;
+	boolean tried = false;
+	boolean stopWaiting = false;
+	boolean isSending = false;
 
+	int previousToken = -1;
+	boolean checking = false;
 
-    @Override
-    public TransmissionInfo TimeslotAvailable(MediumState previousMediumState, int controlInformation, int localQueueLength) {
-        if(state == State.INITIAL) {
-            if(previousMediumState == MediumState.Succes && tried) {
-                System.out.println("Victory!");
-                node = controlInformation;
-                System.out.println(node);
-                state = State.WAITING;
-                return new TransmissionInfo(TransmissionType.Silent, 0);
-            }
-            if(previousMediumState == MediumState.Succes) {
-                System.out.println("Someone won!");
-                count = controlInformation;
-                return new TransmissionInfo(TransmissionType.Silent, 0);
-            }
-            tried = false;
-            if(previousMediumState == MediumState.Idle && waitingTime <= 0) {
-                System.out.println("Lets try!");
-                tried = true;
-                return new TransmissionInfo(TransmissionType.NoData, count + 1);
-            } else if (previousMediumState == MediumState.Collision) {
-                System.out.println("Collide");
-                waitingTime = (int) Math.round(Math.random() * 4);
-                return new TransmissionInfo(TransmissionType.Silent, 0);
-            } else {
-                waitingTime--;
-                System.out.println("Waiting");
-            }
-        } else if(state == State.WAITING) {
-            if(controlInformation == 3 || node == 3) {
-                state = State.SENDING;
-            }
-        } else if (state == State.SENDING) {
-            if((controlInformation  % 4)  != node) {
-                System.out.println("Not my turn!");
-                return new TransmissionInfo(TransmissionType.Silent, 0);
-            } else {
-                if(counter != 0 && localQueueLength != 0) {
-                    counter--;
-                    return new TransmissionInfo(TransmissionType.Data, node);
-                } else if(counter == 0) {
-                    return new TransmissionInfo(TransmissionType.NoData, node+1);
-                }else if(localQueueLength != 0) {
-                    System.out.println("My turn!");
-                    counter = 8;
-                    return new TransmissionInfo(TransmissionType.Data, node);
-                } else {
-                    System.out.println("I'm broke :(");
-                    return new TransmissionInfo(TransmissionType.NoData, node+1);
-                }
-            }
-        }
-        return new TransmissionInfo(TransmissionType.Silent, 0);
-    }
+	
+	
+	
+	public int calculateNewNumber(int counter){
+		if (checking) {
+			int temp = counter;
+			temp = (temp + 1) % 4;
+			if (temp == 3){
+				checking = false;
+			}
+			return temp;
+		} else {
+			int temp = counter;
+			
+			for (int i=0; i<4; i++) {
+				temp = temp + 1 % 4;
+				if (nodesWithQueue[temp]){
+					return temp;
+				}
+			}
+			
+			temp = (temp + 1) % 4;
+			return temp;
+		}
+	}
+	
+	
+	@Override
+	public TransmissionInfo TimeslotAvailable(MediumState previousMediumState, int controlInformation, int localQueueLength) {
+		if(state == State.INITIAL) {
+
+			if (previousMediumState == MediumState.Succes){
+				previousToken = controlInformation;
+				if (tried){
+					node = controlInformation;	
+					state = State.WAITING;
+					System.out.println("IK BEN: "+node);
+					return new TransmissionInfo(TransmissionType.Silent, 0);
+				}else if (controlInformation == 2) {
+					node = 3;
+					state = State.SENDING;
+				}
+			}
+			tried = false;
+
+			if (new Random().nextInt(100) < 33) {
+				tried = true;
+				return new TransmissionInfo(TransmissionType.NoData, previousToken + 1); 
+			}
+		} 
+
+		else if(state == State.WAITING) {
+
+			if (previousMediumState == MediumState.Succes && controlInformation == 2) {
+				state = State.SENDING;
+			}
+			return new TransmissionInfo(TransmissionType.Silent, 0);
+		} 
+		else if (state == State.SENDING) {
+
+			//Bijhouden welke nodes er aan het zenden zijn
+			if (previousMediumState == MediumState.Succes) {
+				int temp = counter;
+				
+				if (controlInformation != HEADER_LAST_PACKET) {
+					nodesWithQueue[temp] = true;
+				} else {
+					nodesWithQueue[temp] = false;
+				}
+			}
+			
+			counter = calculateNewNumber(counter);
+			if (counter == 3) {
+				checking = false;
+			}
+			
+			//Doen als er een collision is
+			if (previousMediumState == MediumState.Collision) {
+				counter = 0;
+				checking = true;
+			}
+			
+			//Laat merken dat je aan het senden bent.
+			if (isSending == false && localQueueLength > 0) {
+				isSending = true;
+				return new TransmissionInfo(TransmissionType.Data, node + 1);
+			}
+			
+			
+			if (node == counter) {
+				if (localQueueLength > 0){
+					System.out.println("SENDING");
+					return new TransmissionInfo(TransmissionType.Data, node + 1);	
+				} else {
+					isSending = false;
+					System.out.println("NO DATA TO SEND");
+					return new TransmissionInfo(TransmissionType.NoData, HEADER_LAST_PACKET);
+				}
+			}
+			System.out.println("NOT MY TURN");
+		}
+		return new TransmissionInfo(TransmissionType.Silent, 0);
+	}
 }
