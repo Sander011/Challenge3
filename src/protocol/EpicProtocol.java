03813fc0111af05ba995edb27246b8e97df6c3d2
Sander011 <sander_meinderts@hotmail.com>
2016-02-18 11:01:35 +0100

    update #12 waiting updated

--- src/protocol/EpicProtocol.java
+++ src/protocol/EpicProtocol.java
@@ -7,21 +7,26 @@ import java.util.Random;
  */
 public class EpicProtocol implements IMACProtocol {
 
-	int waitingTimeSlots;
+
+    private final int MAX_SEND = (int) Math.pow(2, 3);
+    private final int WAIT_TIME = 4;
+
+    int counter;
+    int waitingTimeSlots;
     State state;
-	
-	
-	public EpicProtocol() {
+
+
+    public EpicProtocol() {
         state = State.INITIAL;
-		
-	}
-	
-	
+        counter = 0;
+    }
+
+
     @Override
     public TransmissionInfo TimeslotAvailable(MediumState previousMediumState,
                                               int controlInformation, int localQueueLength) {
 
-        if(state.equals(State.INITIAL)) {
+        if (state.equals(State.INITIAL)) {
             // No data to send, just be quiet
             if (localQueueLength == 0) {
                 System.out.println("SLOT - No data to send.");
@@ -30,10 +35,15 @@ public class EpicProtocol implements IMACProtocol {
                 state = State.FIRSTSEND;
                 System.out.println("Send first packet");
                 return new TransmissionInfo(TransmissionType.Data, localQueueLength);
+            } else if (controlInformation == 12345) {
+                System.out.println("Sending limit reached, try sending");
+                state = State.FIRSTSEND;
+                return new TransmissionInfo(TransmissionType.Data, localQueueLength);
+            } else {
+                System.out.println("Someone is sending packets :(");
+                return new TransmissionInfo(TransmissionType.Silent, 0);
             }
-        }
-
-        else if (state.equals(State.FIRSTSEND)) {
+        } else if (state.equals(State.FIRSTSEND)) {
             //Als deze het eerste pakketje heeft verzonden en er is geen collision, zet sending op true
             if (previousMediumState != MediumState.Collision) {
                 state = State.SENDING;
@@ -45,14 +55,12 @@ public class EpicProtocol implements IMACProtocol {
             else {
                 state = State.WAITING;
                 System.out.println("Start waiting");
-                waitingTimeSlots = (int) Math.round(Math.random() * 4);
+                waitingTimeSlots = (int) Math.round(Math.random() * WAIT_TIME);
                 return new TransmissionInfo(TransmissionType.Silent, 0);
             }
-        }
-
-        else if (state.equals(State.WAITING)) {
+        } else if (state.equals(State.WAITING)) {
             waitingTimeSlots--;
-            if (waitingTimeSlots <= 0 && previousMediumState == MediumState.Idle){
+            if (waitingTimeSlots <= 0 && previousMediumState == MediumState.Idle) {
                 System.out.println("Waited long enough, send!");
                 state = State.FIRSTSEND;
                 return new TransmissionInfo(TransmissionType.Data, localQueueLength);
@@ -60,14 +68,19 @@ public class EpicProtocol implements IMACProtocol {
                 state = State.INITIAL;
                 return new TransmissionInfo(TransmissionType.Silent, 0);
             } else {
-                    System.out.println("Continue waiting");
-                    return new TransmissionInfo(TransmissionType.Silent, 0);
-                }
-        }
-
-        else if (state.equals(State.SENDING)) {
+                System.out.println("Continue waiting");
+                return new TransmissionInfo(TransmissionType.Silent, 0);
+            }
+        } else if (state.equals(State.SENDING)) {
+            counter++;
+            if (counter >= MAX_SEND) {
+                waitingTimeSlots = (int) Math.round(Math.random() * WAIT_TIME);
+                state = State.WAITING;
+                System.out.println("Sending limit reached");
+                return new TransmissionInfo(TransmissionType.Data, 12345);
+            }
             System.out.println("Sending...");
-            if(localQueueLength == 0) {
+            if (localQueueLength == 0) {
                 state = State.INITIAL;
                 System.out.println("Completed current queue");
                 return new TransmissionInfo(TransmissionType.Silent, 0);
@@ -76,7 +89,7 @@ public class EpicProtocol implements IMACProtocol {
         }
 
 
-        System.out.println(state);
+        System.out.println("Meh");
         return new TransmissionInfo(TransmissionType.Silent, 0);
     }
 }
