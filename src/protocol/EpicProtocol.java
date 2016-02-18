a1da45ce5cf7d0b5fdb1967aba2a40a737168668
Sander011 <sander_meinderts@hotmail.com>
2016-02-18 10:23:23 +0100

    update #8 Enum state

--- src/protocol/EpicProtocol.java
+++ src/protocol/EpicProtocol.java
@@ -10,11 +10,13 @@ public class EpicProtocol implements IMACProtocol {
 	boolean firstPacketSent;
 	boolean sending;
 	int waitingTimeSlots;
+    State state;
 	
 	
 	public EpicProtocol() {
 		firstPacketSent = false;
 		sending = false;
+        state = State.INITIAL;
 		
 	}
 	
@@ -22,52 +24,60 @@ public class EpicProtocol implements IMACProtocol {
     @Override
     public TransmissionInfo TimeslotAvailable(MediumState previousMediumState,
                                               int controlInformation, int localQueueLength) {
-        // No data to send, just be quiet
-        if (localQueueLength == 0) {
-            System.out.println("SLOT - No data to send.");
-            firstPacketSent = false;
-            sending = false;
-            return new TransmissionInfo(TransmissionType.Silent, 0);
-        }
-        
-        //Als deze aan het zenden is, blijf doorzenden totdat de queue length 0 is
-        if (sending) {
-            System.out.println("Sending...");
-            return new TransmissionInfo(TransmissionType.Data, localQueueLength);
-        }
-        
-        //Als deze een queue heeft en in het vorige timeslot geen pakketje heeft ontvangen
-        //Verstuur een pakketje
-        if (!firstPacketSent && !sending && (controlInformation == 0 || previousMediumState == MediumState.Idle)) {
-        	firstPacketSent = true;
-            System.out.println("Send first packet");
-            return new TransmissionInfo(TransmissionType.Data, localQueueLength);
+
+        if(state.equals(State.INITIAL)) {
+            // No data to send, just be quiet
+            if (localQueueLength == 0) {
+                System.out.println("SLOT - No data to send.");
+                firstPacketSent = false;
+                sending = false;
+                return new TransmissionInfo(TransmissionType.Silent, 0);
+            } else if (!firstPacketSent && !sending && (controlInformation == 0 || previousMediumState == MediumState.Idle)) {
+                firstPacketSent = true;
+                state = State.FIRSTSEND;
+                System.out.println("Send first packet");
+                return new TransmissionInfo(TransmissionType.Data, localQueueLength);
+            }
         }
 
-        //Als deze het eerste pakketje heeft verzonden en er is geen collision, zet sending op true
-        if (firstPacketSent && previousMediumState != MediumState.Collision) {
-        	sending = true;
-            System.out.println("Start sending");
-            return new TransmissionInfo(TransmissionType.Data, localQueueLength);
-        } 
-        //Als er wel een collisions was, zet de timer op een random getal tussen 0 en 4.
-        //Dan wachten enzo
-        else if (firstPacketSent && previousMediumState == MediumState.Collision) {
-            System.out.println("Start waiting");
-            waitingTimeSlots = (int) Math.round(Math.random() * 4);
-        }
-        
-        //Wachten
-        //Als wachten voorbij, verstuur pakketje
-        if (firstPacketSent && !sending) {
-        	waitingTimeSlots--;
-        	if (waitingTimeSlots <= 0){
-                System.out.println("Waited long enough, send!");
+        else if (state.equals(State.FIRSTSEND)) {
+            //Als deze het eerste pakketje heeft verzonden en er is geen collision, zet sending op true
+            if (firstPacketSent && previousMediumState != MediumState.Collision) {
+                sending = true;
+                state = State.SENDING;
+                System.out.println("Start sending");
                 return new TransmissionInfo(TransmissionType.Data, localQueueLength);
-        	} else {
+            }
+            //Als er wel een collisions was, zet de timer op een random getal tussen 0 en 4.
+            //Dan wachten enzo
+            else if (firstPacketSent && previousMediumState == MediumState.Collision) {
+                state = State.WAITING;
+                System.out.println("Start waiting");
+                waitingTimeSlots = (int) Math.round(Math.random() * 4);
+                return new TransmissionInfo(TransmissionType.Silent, 0);
+            }
+        }
+
+        else if (state.equals(State.WAITING)) {
+            waitingTimeSlots--;
+            if (waitingTimeSlots <= 0){
+                System.out.println("Waited long enough, send!");
+                state = State.FIRSTSEND;
+                return new TransmissionInfo(TransmissionType.Data, localQueueLength);
+            } else {
                 System.out.println("Continue waiting");
                 return new TransmissionInfo(TransmissionType.Silent, 0);
-        	}
+            }
+        }
+
+        else if (state.equals(State.SENDING)) {
+            System.out.println("Sending...");
+            if(localQueueLength == 0) {
+                state = State.INITIAL;
+                System.out.println("Completed current queue");
+                return new TransmissionInfo(TransmissionType.Silent, 0);
+            }
+            return new TransmissionInfo(TransmissionType.Data, localQueueLength);
         }
 
         System.out.println("Meh");
---
+++ src/protocol/State.java
@@ -0,0 +1,8 @@
+package protocol;
+
+/**
+ * Created by Sander on 18/02/2016.
+ */
+public enum State {
+    WAITING, SENDING, INITIAL, FIRSTSEND
+}
