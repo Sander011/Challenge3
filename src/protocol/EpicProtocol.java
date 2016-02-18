0397d794dcb09481b32a9d4c6f76ca7bda9d4db2
Sander011 <sander_meinderts@hotmail.com>
2016-02-18 10:00:40 +0100

    update #6

--- src/protocol/EpicProtocol.java
+++ src/protocol/EpicProtocol.java
@@ -7,24 +7,26 @@ import java.util.Random;
  */
 public class EpicProtocol implements IMACProtocol {
 
+
+
     @Override
     public TransmissionInfo TimeslotAvailable(MediumState previousMediumState,
                                               int controlInformation, int localQueueLength) {
+
+
         // No data to send, just be quiet
         if (localQueueLength == 0) {
             System.out.println("SLOT - No data to send.");
             return new TransmissionInfo(TransmissionType.Silent, 0);
         }
 
-        // MEEEHHHH
-
         // Randomly transmit with 60% probability
         if (new Random().nextInt(100) < 38) {
             System.out.println("SLOT - Sending data and hope for no collision.");
-            return new TransmissionInfo(TransmissionType.Data, 0);
+            return new TransmissionInfo(TransmissionType.Data, localQueueLength);
         } else {
             System.out.println("SLOT - Not sending data to give room for others.");
-            return new TransmissionInfo(TransmissionType.Silent, localQueueLength);
+            return new TransmissionInfo(TransmissionType.Silent, 0);
         }
 
     }
